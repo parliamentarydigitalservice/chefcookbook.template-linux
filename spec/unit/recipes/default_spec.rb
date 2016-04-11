006982fab1a2f2730c17f334aa9cd1ddb779d1cd
Vinitha Devadas <Vinitha Devadas>
2016-04-11 15:29:44 +0100

    Added ChefSpec tests around node overrides

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,15 +1,28 @@
 #
-# Cookbook Name:: core
+# Cookbook Name:: core-linux
 # Spec:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'core::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+describe 'core-linux::default' do
+  let(:chef_run) do
+    runner = ChefSpec::SoloRunner.new
+    runner.converge(described_recipe)
+  end
 
-  it 'includes the `chocolatey` recipe' do
-    expect(chef_run).to include_recipe('chocolatey::default')
+  it 'includes the `chef-client` recipe' do
+    expect(chef_run).to include_recipe('chef-client::default')
+  end
+
+  # Testing chef_client interval override
+  it 'overrides interval' do
+    expect(chef_run.node['chef_client']['interval']).to eq('1800')
+  end
+
+  # Testing chef_client config log_level override
+  it 'overrides log config' do
+    expect(chef_run.node['chef_client']['config']['log_level']).to eq('warn')
   end
 end
