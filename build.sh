29c17177a6b6e00438e21b77c6d30cd0de17a9a7
Steven Wade <steven@stevenwade.co.uk>
2016-04-13 13:06:00 +0100

    moving the AMIs over to sit in Ireland.

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ## Execute the CI task from the Rakefile
-sudo $(which chef) exec rake ci
+#sudo $(which chef) exec rake ci
 
 ## Obtain the source AMI to use for our Packer build.
-## ubuntu-trusty-14.04-amd64-server-20160114.5
-base_ami_id='ami-9abea4fb'
+## ubuntu/images/hvm/ubuntu-trusty-14.04-amd64-server-20160406
+base_ami_id='ami-6077f713'
 
 ## Build an AMI for this cookbook
 $(which chef) exec rake packer[$base_ami_id]
