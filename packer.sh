992b1810c3643de4652133fb955f53e3883c6cee
James McKay <code@jamesmckay.net>
2016-12-14 14:30:28 +0000

    DRY up the declaration of the AWS region.
    Currently set to us-east-1 for testing purposes.

--- packer.sh
+++ packer.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+export AWS_REGION=us-east-1
+
 ## Obtain the source AMI to use for our Packer build.
 ## ubuntu/images/hvm/ubuntu-trusty-14.04-amd64-server-20160406
-base_ami_id='ami-6077f713'
+base_ami_id=$(aws ec2 describe-images \
+    --filters Name=name,Values="ubuntu/images/hvm/ubuntu-trusty-14.04-amd64-server-20160406" \
+    --region $AWS_REGION \
+    --output text \
+    --query "Images[*].ImageId" \
+)
+
+echo $base_ami_id
 
 ## Build an AMI for this cookbook
 $(which chef) exec rake packer[$base_ami_id]
